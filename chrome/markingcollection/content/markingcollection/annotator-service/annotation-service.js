440c5c29836da081dc1b9aa22756ef85fff768d9
ssvw <sroth>
2013-07-24 09:22:29 +0000

    Retrieve current value of REST URI from Settings Dialog.
    git-svn-id: https://svn.clarin.eu/DASISH/t5.6/client/trunk@3185 72633d89-856b-0410-a642-59178932f03a

--- chrome/markingcollection/content/markingcollection/annotator-service/annotation-service.js
+++ chrome/markingcollection/content/markingcollection/annotator-service/annotation-service.js
@@ -1,5 +1,17 @@
 var annotationFramework = (function() {
+    // default value for REST service
     var backend = "http://pelle.ssd.gu.se:8080/exist/rest/db/annotation-framework/api/annotations.xql";
+    var selected_state = nsPreferences.copyUnicharPref("wiredmarker.marker.initdata.url","");
+    // selected_state values: "" (empty value as long as Settings Dialog has not been opened) OR "WM_REST_DEFAULT" OR "WM_REST_UserDefined"
+    // user-specified value for REST service
+    if (selected_state === "WM_REST_UserDefined") {
+        var backend = nsPreferences.copyUnicharPref("wiredmarker.marker.initdata.url_format","");
+    }
+    alert("The following backend is used: " + backend);
+    // TODO: The above code is executed only when restarting the application (or when a new window is opened),
+    // however the settings will be changed without calling for a restart (or opening a new window).
+    // Solution to get the correct backend URL at any time?
+    
     return {
         getAnnotations: function(params, callback) {
             $.ajax({
