4428e0b101da9ec17b610a76075adc813c3aa1db
ssvw <sroth>
2013-08-06 12:30:44 +0000

    Mockjax files and test files to mock a REST Backend using mockjax, ajax and jQuery. Initial commit.
    git-svn-id: https://svn.clarin.eu/DASISH/t5.6/client/trunk@3252 72633d89-856b-0410-a642-59178932f03a

---
+++ chrome/markingcollection/content/markingcollection/annotator-service/test/mockjax/mockjax-xml.js
@@ -0,0 +1,49 @@
+/*
+ * @content Mocking retrieval of annotations
+ * (see: https://trac.clarin.eu/wiki/DASISH/XSD%20and%20XML#RespondingGETapiannotationslinkhttp:en.wikipedia.orgwikiSagrada_FamC3ADliaaccessread;
+ * Scenario - visiting an annotated web page: https://trac.clarin.eu/wiki/DASISH/Scenario)
+ */
+
+var url = '/api/annotations?link="http://en.wikipedia.org/wiki/Sagrada_Fam%C3%ADlia"';
+
+$.mockjax({
+    /* url: A string or regular expression specifying the url of the request that the data should be mocked for. */
+    // url: '/api/annotations?link="http://en.wikipedia.org/wiki/Sagrada_Fam%C3%ADlia"&access=read',
+    url: url,
+    /* type: Specify what HTTP method to match, usually GET or POST. Case-insensitive, so get and post also work. */
+    type: 'GET',
+    /* dataType: Allowed data formats are Text, HTML, JSON, Script and XML. */
+    dataType: 'xml',
+    /* responseTime: An integer that specifies a simulated network and server latency (in milliseconds). */
+    responseTime: 750,
+    /* proxy: A string specifying a path to a file, from which the contents will be returned for the request. */
+    proxy: 'mocks/annotations-sagrada.xml'
+});
+
+var XMLData = function() {
+    return {
+        getMockAnnotations: function(url) {
+            $.ajax({
+                url: url,
+                dataType: 'xml',
+                type: 'GET',
+                success: function(xml, textStatus, jqXHR) {
+
+                    console.log('Data: ' + $(xml));
+                    console.log($(xml));
+                    console.log('Status: ' + textStatus);
+                    console.log('XMLHttpRequest object: ' + jqXHR);
+                    console.log(jqXHR);
+
+                    var headline = "";
+                    $(xml).find('annotation').each(function() {
+                        headline += '<br />' + $(this).find('headline').text();
+                    });
+                    $('#xmlContents').html('<h4 style="margin-bottom: 0;">Headline:</h4>' + headline);
+                }
+            });
+        }
+    }
+}();
+
+XMLData.getMockAnnotations(url);
