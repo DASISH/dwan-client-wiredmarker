bd90cf2c5bf450aa8d3f12bbe81a652f549e222a
ssvw <sroth>
2013-08-08 11:13:12 +0000

    Removed extra "/" from owner ref attribute.
    git-svn-id: https://svn.clarin.eu/DASISH/t5.6/client/trunk@3323 72633d89-856b-0410-a642-59178932f03a

--- chrome/markingcollection/content/markingcollection/annotator-service/conversion.utils.js
+++ chrome/markingcollection/content/markingcollection/annotator-service/conversion.utils.js
@@ -79,7 +79,7 @@ function om_object2annotation(om_object){
                             xsi:schemaLocation="http://www.dasish.eu/ns/addit http://dasish.eu/DASISH-schema.xsd"\n\
                             URI="tempAID'+om_object.oid+'"\n\
                             timeStamp="'+timestamp.toISOString()+'">\n\
-                        <owner ref="http://dasish.eu/users//UIxyz/how_will_this_be_sent_from_the_client"/>\n\
+                        <owner ref="http://dasish.eu/users/UIxyz/how_will_this_be_sent_from_the_client"/>\n\
                         <headline>'+om_object.oid_title+'</headline>\n\
                         <body type="Note" ref="tmp'+om_object.oid+'">\n\
                             <xhtml:span style="'+style+'">'+note+'</xhtml:span>\n\
