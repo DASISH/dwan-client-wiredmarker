0a9152a464cfb5195e0c970dbcea47839039f7e1
ssvw <sroth>
2013-08-09 09:57:57 +0000

    Updated mock functionality and file contents.
    git-svn-id: https://svn.clarin.eu/DASISH/t5.6/client/trunk@3340 72633d89-856b-0410-a642-59178932f03a

--- chrome/markingcollection/content/markingcollection/annotator-service/test/mockjax/mockjax-xml.js
+++ chrome/markingcollection/content/markingcollection/annotator-service/test/mockjax/mockjax-xml.js
@@ -6,10 +6,12 @@
 
 var url = '/api/annotations?link="http://en.wikipedia.org/wiki/Sagrada_Fam%C3%ADlia"';
 
+// Mock for Responding GET api/annotations?link="URI"
 $.mockjax({
     /* url: A string or regular expression specifying the url of the request that the data should be mocked for. */
     // url: '/api/annotations?link="http://en.wikipedia.org/wiki/Sagrada_Fam%C3%ADlia"&access=read',
     url: url,
+    // url: '/api/annotations?link="http://en.wikipedia.org/wiki/Sagrada_Fam%C3%ADlia"',
     /* type: Specify what HTTP method to match, usually GET or POST. Case-insensitive, so get and post also work. */
     type: 'GET',
     /* dataType: Allowed data formats are Text, HTML, JSON, Script and XML. */
@@ -17,7 +19,25 @@ $.mockjax({
     /* responseTime: An integer that specifies a simulated network and server latency (in milliseconds). */
     responseTime: 750,
     /* proxy: A string specifying a path to a file, from which the contents will be returned for the request. */
-    proxy: 'mocks/annotations-sagrada-GET.xml'
+    proxy: 'mocks/annotations-sagrada-GET.xml',
+});
+
+// Mock for Responding GET api/annotations/AID20130808114716 (annot-0001-GET.xml)
+$.mockjax({
+    url: '/api/annotations/AID20130808114716',
+    type: 'GET',
+    dataType: 'xml',
+    responseTime: 750,
+    proxy: 'mocks/annot-0001-GET.xml',
+});
+
+// Mock for Responding GET api/annotations/AID20130808124131 (annot-0002-GET.xml)
+$.mockjax({
+    url: '/api/annotations/AID20130808124131',
+    type: 'GET',
+    dataType: 'xml',
+    responseTime: 750,
+    proxy: 'mocks/annot-0002-GET.xml',
 });
 
 var XMLData = function() {
@@ -28,9 +48,9 @@ var XMLData = function() {
                 dataType: 'xml',
                 type: 'GET',
                 success: function(xml, textStatus, jqXHR) {
-                // success: function(xml) {    
-                // can be used if parameters textStatus and jqXHR are not required within the following code block
-                    
+                    // success: function(xml) {    
+                    // can be used if parameters textStatus and jqXHR are not required within the following code block
+
                     $xml = $(xml);
                     
                     console.log('Data: ' + $xml);
@@ -44,15 +64,38 @@ var XMLData = function() {
                     $xml.find('annotations annotation').each(function() {
                         aidArr.push($(this).attr('ref').substr(29)); // http://dasish.eu/annotations/ is erased
                     });
-  
+
                     console.log(aidArr);
-                    
-                    for(var i in aidArr) {
+
+                    for (var i in aidArr) {
                         aid_string += '<br />' + aidArr[i];
                         $('#xmlContents').html('<h4 style="margin-bottom: 0;">List of AIDs returned from REST service address<br />GET api/annotations?link="URI":</h4>' + aid_string);
+                        url = "";
+                        url = '/api/annotations/' + aidArr[i];
+                        
+                        $.ajax({
+                            url: url,
+                            dataType: 'xml',
+                            type: 'GET',
+                            success: function(xml) {
+                                
+                                $xml = $(xml);
+   
+                                var body = "";
+                                $xml.find('annotation').each(function() {
+                                    body = $(this).find('body').text();
+  
+                                    $('#xmlContents').append("<br /><br /><strong>Body content of " + aidArr[i] + ":</strong><br /><em>" + body + "</em>");
+                                    
+                                });   
+                                
+                            }
+                        });
+
                     }
-                    
+
                 }
+
             });
         }
     }
