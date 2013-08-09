5646e6b8b23b6a1166740e5fa7a639bc7c30672a
Olof Olsson <borsna@gmail.com>
2013-08-09 09:42:57 +0000

    get default-functions
    git-svn-id: https://svn.clarin.eu/DASISH/t5.6/client/trunk@3339 72633d89-856b-0410-a642-59178932f03a

--- chrome/markingcollection/content/markingcollection/annotator-service/annotation-service.js
+++ chrome/markingcollection/content/markingcollection/annotator-service/annotation-service.js
@@ -1,22 +1,27 @@
 var annotationFramework = (function() {
     // default value for REST service
-    var backend = "http://pelle.ssd.gu.se:8080/exist/rest/db/annotation-framework/api/annotations.xql";
-    var selected_state = nsPreferences.copyUnicharPref("wiredmarker.marker.initdata.url","");
-    // selected_state values: "" (empty value as long as Settings Dialog has not been opened) OR "WM_REST_DEFAULT" OR "WM_REST_UserDefined"
-    // user-specified value for REST service
-    if (selected_state === "WM_REST_UserDefined") {
-        var backend = nsPreferences.copyUnicharPref("wiredmarker.marker.initdata.url_format","");
-    }
-    // alert("The following backend is used: " + backend);
-    // TODO: The above code is executed only when restarting the application (or when a new window is opened),
-    // however the settings will be changed without calling for a restart (or opening a new window).
-    // Solution to get the correct backend URL at any time?
-    
+    var default_backend = "http://pelle.ssd.gu.se:8080/exist/rest/db/annotation-framework/api/annotations.xql";
+        
     return {
+        getDefaultBackend: function(){
+            return default_backend;
+        },
+                
+        getBackend: function(){
+            // selected_state values: "" (empty value as long as Settings Dialog has not been opened) OR "WM_REST_DEFAULT" OR "WM_REST_UserDefined"
+            // user-specified value for REST service
+            var selected_state = nsPreferences.copyUnicharPref("wiredmarker.marker.initdata.url","");
+            if (selected_state === "WM_REST_UserDefined") {
+                return nsPreferences.copyUnicharPref("wiredmarker.marker.initdata.url_format","");
+            }else{
+                return default_backend;
+            }
+        },
+        
         getAnnotations: function(params, callback) {
             $.ajax({
                 type: "GET",
-                url: this.backend,
+                url: this.getBackend()+'/annotations',
                 data:params,
                 dataType: "xml",
                 success: function(xml) {
@@ -34,11 +39,27 @@ var annotationFramework = (function() {
                 }
             });
         },
-        putAnnotation: function() {
+        getAnnotation: function(aid, callback){
+            $.ajax({
+                type: "GET",
+                url: this.getBackend()+'/'+aid,
+                dataType: "xml",
+                success: function(xml) {
+                    
+                    $xml.find('annotation').each(function(){              
+                        var annotation = $.xml2json(this);
+                    });
+
+                    callback.call(undefined, annotation);
+                }
+            });
+        },
+        putAnnotation: function(annotation) {
             $.ajax({
                 type: "POST",
-                url: this.backend,
-                dataType: "xml"
+                url: this.getBackend()+'?store=true',
+                dataType: "xml",
+                data:annotation
             });
         },
         setBackend:function(url) {this.backend = url;}
