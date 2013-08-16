e6318150556a623667a3257691d302bac597497d
Olof Olsson <borsna@gmail.com>
2013-08-16 14:38:54 +0000

    translation of annotation to om_object first draft done
    git-svn-id: https://svn.clarin.eu/DASISH/t5.6/client/trunk@3403 72633d89-856b-0410-a642-59178932f03a

--- chrome/markingcollection/content/markingcollection/annotator-service/conversion.utils.js
+++ chrome/markingcollection/content/markingcollection/annotator-service/conversion.utils.js
@@ -20,19 +20,53 @@ function annotation2om_object(annotation){
                         oid_date        :""
                     };
     
+    //example anchor:
+    // http://snd.gu.se/#hyperanchor1.3://div[@id="node-170"]/div[1]/div[1]/div[1]/div[1]/p[1](294)(3)(sse)&
+    // //div[@id="node-170"]/div[1]/div[1]/div[1]/div[1]/p[1](365)(3)(kav)&
+    // border:thin dotted rgb(255,204,0);background-color:rgb(255,255,204);color:rgb(0,0,0); 
+    
+    var title = $(annotation).find('headline').text().trim();
+    var body = $(annotation).find('body').text().trim();
+    var style = $(annotation).find('body').find('xhtml\\:span').attr('style');
+    var type = $(annotation).find('body').attr('type').toLowerCase();
+    var link = $(annotation).find('link').text();
+    var time = $(annotation).attr('timeStamp');
+       
+    var urlParts = link.split("#xpointer"); 
+    
+    om_object.doc_url = urlParts[0];
+    om_object.con_url = urlParts[0];
+    var xpointer = urlParts[1];
+    
+    //start dom
+    xpointer.match(/start-point\(string-range\((.+?)\)\)\/range-to\(/);
+    var parts = RegExp.$1.split(',');
+    om_object.bgn_dom = parts[0]+'('+parts[2]+')(3)';
+    
+    //end dom
+    xpointer.match(/range-to\(string-range\((.+?)\)\)\)/);
+    var parts = RegExp.$1.split(',');
+    om_object.end_dom = parts[0]+'('+parts[2]+')(3)';
+    
     //start xpath in xpointer: (?<=start-point\(string-range\()(.*\n?)(?=\,'')
     //end xpath in xpointer: (?<=range-to\(string-range\()(.*\n?)(?=\,'')
     
+    //build hyperanchor
+    var hyperanchor = '#hyperanchor1.3:'+om_object.bgn_dom+'&'+om_object.end_dom+'&'+style;
     
-    om_object.oid = hashCode(annotation['xml:id']);
-    console.log(annotation.targetSources.targetSource);
-    om_object.oid_property = "<PROPERTY><HYPER_ANCHOR>"+annotation.targetSources.targetSource.id+"</HYPER_ANCHOR><NOTE>"+annotation.body+"</NOTE></PROPERTY>";
     
-    if(annotation.type.toLowerCase() === 'note'){
-        om_object.oid_txt = annotation.body;
+    
+    om_object.doc_title = title;
+    
+    om_object.oid = hashCode($(annotation).attr('ref'));
+    console.log(link);
+    om_object.oid_property = "<PROPERTY><HYPER_ANCHOR>"+om_object.doc_url+hyperanchor+"</HYPER_ANCHOR><NOTE>"+body+"</NOTE></PROPERTY>";
+    
+    if(type === 'note'){
+        om_object.oid_txt = body;
         om_object.oid_type = 'text';
     }
-    var d = new Date(annotation.timeStamp);
+    var d = new Date(time);
     om_object.oid_date = (d.getMonth()+1)+'/'+d.getDate()+'/'+d.getFullYear()+' '+d.getHours()+':'+d.getMinutes()+':'+d.getSeconds();
     
     return om_object;
@@ -99,7 +133,7 @@ function om_object2annotation(om_object){
 
 function hashCode(str){
       var hash = 0;
-      if (str.length == 0) return hash;
+      if (str == undefined || str.length == 0) return hash;
       for (i = 0; i < str.length; i++) {
           char = str.charCodeAt(i);
           hash = ((hash<<5)-hash)+char;
