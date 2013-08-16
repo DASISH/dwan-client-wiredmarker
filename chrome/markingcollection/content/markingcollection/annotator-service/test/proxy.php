7cf65a9cce8fb86ee48a118d66484b028c8714c4
Olof Olsson <borsna@gmail.com>
2013-08-16 14:38:37 +0000

    translation of annotation to om_object first draft done
    git-svn-id: https://svn.clarin.eu/DASISH/t5.6/client/trunk@3402 72633d89-856b-0410-a642-59178932f03a

--- chrome/markingcollection/content/markingcollection/annotator-service/test/proxy.php
+++ chrome/markingcollection/content/markingcollection/annotator-service/test/proxy.php
@@ -11,6 +11,12 @@
   if($_REQUEST['log']){
       file_put_contents('log.txt', date("c")."\t".$_REQUEST['message']."\n",  FILE_APPEND);
   }
+  
+  if($_GET['store']){
+      $postdata = file_get_contents("php://input");
+      file_put_contents('log.txt', date("c")."\tstore in: ".date('Y-m-d_H.i.s').".xml\n",  FILE_APPEND);
+      file_put_contents(date('Y-m-d_H.i.s').'.xml', $postdata);
+  }
 
   $response = file_get_contents($_GET['url'].'?'.$params);
 
