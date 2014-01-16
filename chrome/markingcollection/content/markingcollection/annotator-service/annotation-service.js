3404ff5a52528fd43fc690e7266c0fd5c7c5c87a
ssvw <sroth>
2014-01-16 12:11:34 +0000

    Adjusted function postAnnotation.
    git-svn-id: https://svn.clarin.eu/DASISH/t5.6/client/trunk@4272 72633d89-856b-0410-a642-59178932f03a

--- chrome/markingcollection/content/markingcollection/annotator-service/annotation-service.js
+++ chrome/markingcollection/content/markingcollection/annotator-service/annotation-service.js
@@ -1,36 +1,34 @@
 var annotationFramework = (function() {
     // default value for REST service
     var default_backend = "http://pelle.ssd.gu.se:8080/exist/rest/db/annotation-framework/api/annotations.xql";
-        
+
     return {
-        getDefaultBackend: function(){
+        getDefaultBackend: function() {
             return default_backend;
         },
-                
-        getBackend: function(){
+        getBackend: function() {
             // selected_state values: "" (empty value as long as Settings Dialog has not been opened) OR "WM_REST_DEFAULT" OR "WM_REST_UserDefined"
             // user-specified value for REST service
-            var selected_state = nsPreferences.copyUnicharPref("wiredmarker.marker.initdata.url","");
+            var selected_state = nsPreferences.copyUnicharPref("wiredmarker.marker.initdata.url", "");
             if (selected_state === "WM_REST_UserDefined") {
-                return nsPreferences.copyUnicharPref("wiredmarker.marker.initdata.url_format","");
-            }else{
+                return nsPreferences.copyUnicharPref("wiredmarker.marker.initdata.url_format", "");
+            } else {
                 return default_backend;
             }
         },
-        
         getAnnotations: function(params, callback) {
             $.ajax({
                 type: "GET",
-                url: this.getBackend()+'/annotations',
-                data:params,
+                url: this.getBackend() + '/annotations',
+                data: params,
                 dataType: "xml",
                 success: function(xml) {
                     var annotations = Array();
 
                     $xml = $(xml);
-                    $xml.find('annotation').each(function(){              
+                    $xml.find('annotation').each(function() {
                         var annotation = $.xml2json(this);
-                        
+
                         annotations.push(annotation);
                     });
 
@@ -38,26 +36,36 @@ var annotationFramework = (function() {
                 }
             });
         },
-        getAnnotation: function(aid, callback){
+        getAnnotation: function(aid, callback) {
             $.ajax({
                 type: "GET",
-                url: this.getBackend()+'/'+aid,
+                url: this.getBackend() + '/' + aid,
                 dataType: "xml",
-                success: function(xml) {         
+                success: function(xml) {
                     var annotation = $xml.find('annotation');
-                    
+
                     callback.call(undefined, annotation);
                 }
             });
         },
         postAnnotation: function(annotation) {
-            $.ajax({
+            return $.ajax({
                 type: "POST",
-                url: this.getBackend()+'?store=true',
+                url: this.getBackend() + '/api/annotations?store=true',
                 dataType: "xml",
-                data:annotation
+                data: annotation,
+                contentType: "application/xml",
+                complete: function(jqXHR, status, responseText) {
+                    if (typeof Firebug !== 'undefined' && Firebug.Console) {
+                        Firebug.Console.log("+ + + + + + + + + + + + + + + + + + + + + + + +");
+                        Firebug.Console.log("Status Code: " + jqXHR.status);
+                        Firebug.Console.log("Response Body: " + jqXHR.responseText);
+                    }
+                }
             });
         },
-        setBackend:function(url) {this.backend = url;}
+        setBackend: function(url) {
+            this.backend = url;
+        }
     }
 }());
