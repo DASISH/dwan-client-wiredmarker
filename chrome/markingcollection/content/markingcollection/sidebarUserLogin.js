48313725c01ddf8bcf93ffb04bc6c1ea9290dd71
ssvw <stephanie.roth@snd.gu.se>
2014-03-19 08:57:30 +0000

    Element styling and functionality for result === 401.
    git-svn-id: https://svn.clarin.eu/DASISH/t5.6/client/trunk@4760 72633d89-856b-0410-a642-59178932f03a

--- chrome/markingcollection/content/markingcollection/sidebarUserLogin.js
+++ chrome/markingcollection/content/markingcollection/sidebarUserLogin.js
@@ -1,21 +1,44 @@
 var UserLogin = {
     checkUserLogin: function() {
         var user = annotationProxy.getLoggedInInfo(function(result) {
-            if (result !== 401) {
-                value = "You are logged in as " + user.dislayName + ".";
-                var loginInfo = document.getElementById("loginInfo");
-                var newLabel = document.createElement("label");
-                newLabel.setAttribute("value", value);
-                newLabel.setAttribute("class", "loggedIn");
-                loginInfo.appendChild(newLabel);
-            } else {
-                value = "Please sign in to the DASISH annotation server.";
-                var loginInfo = document.getElementById("loginInfo");
-                var newLabel = document.createElement("label");
-                newLabel.setAttribute("value", value);
-                newLabel.setAttribute("class", "loginLabel");
-                newLabel.setAttribute("onclick", "UserLogin.openPopup()");
-                loginInfo.appendChild(newLabel);
+            // TODO: Do we need to refine the status code checks?
+
+            // user is logged in
+            if (result === 200) {
+
+                /*
+                 value = "You are logged in as " + user.dislayName + ".";
+                 var loginInfo = document.getElementById("loginInfo");
+                 var newLabel = document.createElement("label");
+                 newLabel.setAttribute("value", value);
+                 newLabel.setAttribute("id", "loggedIn");
+                 loginInfo.appendChild(newLabel);
+                 */
+
+                // user is not logged in
+            } else if (result === 401) {
+
+                // Label element with status information
+                var loginStatus = document.getElementById("loginStatus");
+                var value = "Please sign in to the DASISH annotation server.";
+                loginStatus.setAttribute("value", value);
+                loginStatus.setAttribute("class", "notLoggedIn");
+                // TODO: Flexible width style or other solution?
+                loginStatus.setAttribute("width", "300");
+
+                // Button for login into backend
+                var loginButton = document.getElementById("loginButton");
+                loginButton.setAttribute("label", "Login");
+                loginButton.setAttribute("class", "loginButton");
+                loginButton.setAttribute("width", "300");
+
+                // Logout button hidden
+                var logoutButton = document.getElementById("logoutButton");
+                logoutButton.setAttribute("hidden", true);
+
+            } else { // server connection timeout, server error 500+, or other
+                // TODO: Refine alert. Maybe some type of XUL window? Should the server admin mail address be given?
+                alert("There might be some trouble with the server connection for " + annotationFramework.getBackend() + ". Please try again later or ask your server administrator for assistance.");
             }
         });
     },
