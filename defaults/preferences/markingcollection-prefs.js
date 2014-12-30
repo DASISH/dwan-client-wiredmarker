a11b1fd4815ddeafdf18ca79473bc45179ccf0a6
ssvw <stephanie.roth@gu.se>
2014-12-30 15:49:08 +0100

    Added path to property file with default server path value.

--- defaults/preferences/markingcollection-prefs.js
+++ defaults/preferences/markingcollection-prefs.js
@@ -1,2 +1,3 @@
 // pref("extensions.{e36db930-f18d-4449-b45f-e286cfb9e03a}.description", "chrome://markingcollection/locale/about.properties");
 pref("extensions.webannotator@dasish.eu.description", "chrome://markingcollection/locale/about.properties");
+pref("extensions.webannotator@dasish.eu.default_server", "chrome://markingcollection/locale/about.properties");
